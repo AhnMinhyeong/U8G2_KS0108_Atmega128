b6f84d69c57dfdcaa8077d156c65541f91a0bb78
AhnMinhyeong <44267756+AhnMinhyeong@users.noreply.github.com>
2018-11-02 04:11:30 +0900

    Update main.c

--- main.c
+++ main.c
@@ -15,12 +15,12 @@ int main(void)
 	port_init();
 	DDRD=0x88;
 	PORTD=0x70;
-	
+	ks0108_off();
 	ks0108_on();
 	u8g2_Setup_ks0108_128x64_f(&u8g2_ks0108, U8G2_R0, u8x8_byte_ks0108, ks0108_GPIO);
 	u8g2_SetFont(&u8g2_ks0108,u8g2_font_ncenB08_tf);
 	u8g2_ClearBuffer(&u8g2_ks0108);
-	u8g2_DrawStr(&u8g2_ks0108, 0, 20, "Hello");
+	u8g2_DrawStr(&u8g2_ks0108, 70, 20, "Hello");
 	u8g2_SendBuffer(&u8g2_ks0108);
     /* Replace with your application code */
     while (1) 
@@ -39,8 +39,7 @@ int main(void)
 		    case 0x50:
 		    PORTD|=(1<<7);
 			u8g2_ClearBuffer(&u8g2_ks0108);
-			u8g2_DrawStr(&u8g2_ks0108, 0, 50, "Hello");
-			u8g2_SendBuffer(&u8g2_ks0108);
+			u8g2_SendBuffer(&u8g2_ks0108);			
 		    _delay_ms(100);
 			break;
 			
